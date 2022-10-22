f185e6b356a3ee34725403b1bdd0840692879dca
Yegor Kuznetsov <yegor@tydbits.com>
2022-10-22 15:59:09 -0700

    v0.1 -- initial commit

---
+++ nca.py
@@ -0,0 +1,197 @@
+import torch
+import torch.nn as nn
+
+#import matplotlib.pyplot as plt
+#from tqdm.auto import tqdm
+import numpy as np
+import random
+import requests
+
+from PIL import Image
+import io
+
+
+SOBEL_X = torch.tensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=torch.float32) / 8.0
+SOBEL_Y = SOBEL_X.T
+
+def conv2d(grid, kernel):
+    """Utility function to perform a simple per-channel depthwise 2d
+    convolution of a filter over a (B, H, W, C) tensor"""
+    ch = grid.shape[-3]
+    return torch.conv2d(grid, kernel.repeat(ch, 1, 1, 1), padding=1, groups=ch)
+
+class NCA(nn.Module):
+    def __init__(self, ch=16):
+        super().__init__()
+
+        self.NCA_channels = ch
+        self.main = nn.Sequential(
+            nn.Conv2d(ch*3, ch*8, 1),
+            nn.ReLU(),
+            nn.Conv2d(ch*8, ch, 1, bias=True),
+        )
+
+        with torch.no_grad():
+            for p in self.main[-1].parameters():
+                p.zero_()
+    
+    def forward(self, grid):
+        perception = torch.cat([
+            grid, conv2d(grid, SOBEL_X), conv2d(grid, SOBEL_Y)
+        ], dim=-3)
+        
+        return self.main(perception)
+
+
+
+def load_emoji(emoji):
+    code = hex(ord(emoji))[2:].lower()
+    r = requests.get(f'https://github.com/googlefonts/noto-emoji/blob/main/png/128/emoji_u{code}.png?raw=true')
+    img = Image.open(io.BytesIO(r.content))
+    img.thumbnail((48, 48), Image.ANTIALIAS)
+    return img
+
+
+
+class Environment:
+    """Utility class to handle growing and training NCA model(s).
+
+    It handles everything to do with inputs and outputs and how to run the
+    models. It could be considered the "world" or "environment".
+
+    This specific version implementes having inputs map directly to a known
+    target output. For the sake of speed, a number of samples is precomputed
+    at initialization time. For the original GrowingNCA, a single example
+    is all that's needed.
+    """
+    def __init__(self, update_p=0.5, life_threshold=0.1, examples_n=1,
+                 NCA_channels=16, size=64, device='cpu', batch_size=16,
+                 loss_func=nn.MSELoss(reduction='mean'),
+                 seed=None, target=None, # np.array (k, size, size) or an emoji
+                 emoji_padding=8,
+                 ):
+        self.update_p = update_p
+        self.life_threshold = life_threshold
+
+        self.NCA_channels = NCA_channels
+        self.size = size
+        self.batch_size = batch_size
+        self.loss_func = loss_func
+
+        self.seed = seed
+        if self.seed is None:
+            self.seed = np.zeros((NCA_channels, size, size), dtype=np.float32)
+            self.seed[:, size//2, size//2] = 1.
+        self.target = target
+        if type(self.target) is str:
+            img = load_emoji(self.target)
+            self.target = np.zeros((4, size, size), dtype=np.float32)
+            self.target[:4, emoji_padding:size-emoji_padding, emoji_padding:size-emoji_padding] = np.array(img).transpose(2, 0, 1) / 255.
+            self.target[:3] *= self.target[3]
+
+        samples = []
+        for _ in range(examples_n):
+            samples.append(self.create_example())
+
+        inputs, outputs = zip(*samples)
+        self.input_samples = torch.from_numpy(np.array(inputs)).to(device)
+        self.output_samples = torch.from_numpy(np.array(outputs)).to(device)
+
+
+    def iterate_NCA(self, model, grid):
+        assert model.NCA_channels == self.NCA_channels
+        update = model(grid)
+        if self.update_p < 1:
+            update_mask = (torch.rand(64, 64) < self.update_p)
+            update *= update_mask
+        grid = grid + update
+
+        if self.life_threshold > 0:
+            alive_mask = (torch.max_pool2d(grid, kernel_size=3,
+                                           stride=1, padding=1) > self.life_threshold)
+            grid = grid*alive_mask
+        return grid
+
+
+    def train_episode(self, model, optimizer, its):
+        grids, targets = self.get_sample()
+
+        optimizer.zero_grad()
+        for _ in range(random.randint(*its)):
+            grids = self.iterate_NCA(model, grids)
+        
+        loss = self.evaluate_results(grids, targets)
+        loss.backward()
+        optimizer.step()
+        return grids, loss.item()
+
+
+    def get_sample(self):
+        indices = np.random.choice(
+                range(len(self.input_samples)), self.batch_size).tolist()
+        batch_in = self.input_samples[indices]
+        batch_out = self.output_samples[indices][:, :3]
+
+        return batch_in, batch_out
+
+
+    def evaluate_results(self, grids, targets):
+        k = targets.shape[1]
+        return self.loss_func(grids[:, :k], targets).mean()
+
+    def create_example(self):
+        # return (input, output)
+        #   input: np array (NCA_channels, size, size) float32
+        #   output: np array (k, size, size) float32
+        if self.target is None:
+            raise TypeError("target cannot be None")
+        return self.seed, self.target
+
+
+def to_pil(grid, vmax=1.0):
+    if type(grid) is torch.Tensor:
+        grid = grid.detach().numpy()
+
+    grid = (grid / vmax * 255).clip(0,255).astype(np.uint8)
+    if len(grid.shape) == 2:
+        return Image.fromarray(grid, mode="L")
+
+    if len(grid.shape) == 3:
+        if grid.shape[0] == 1:
+            return Image.fromarray(grid[0], mode="L")
+        if 3 <= grid.shape[0] <= 4:
+            return Image.fromarray(grid[:3, :, :].transpose(1,2,0), mode="RGB")
+
+    raise TypeError("invalid image or something")
+
+
+def visualize(grid, w_im, vmax=1.0, size=256):
+    image = to_pil(grid, vmax=vmax)
+    image = image.resize((size, size), resample=Image.Resampling.NEAREST)
+    f = io.BytesIO()
+    image.save(f, 'jpeg')
+    w_im.value = f.getvalue()
+
+
+#class TargetEnvironment(Environment):
+#    """Environment with a well-defined input mapped to a complete target output."""
+#    def __init__(self, **kwargs):
+#        super().__init__(**kwargs)
+#
+#    def evaluate_results(self, grids, targets):
+#        raise NotImplementedError()
+#        #return self.loss_func(grids, targets).mean()
+#
+#    def create_example(self):
+#        # return (input, output)
+#        #   input: np array (NCA_channels, size, size) float32
+#        #   output: np array (k, size, size) float32
+#        raise NotImplementedError()
+#
+#
+#class ElectrodeEnvironment(Environment):
+#    """Environment which implements external application of I/O."""
+#    def __init__(self, **kwargs):
+#        super().__init__(**kwargs)
+
+
